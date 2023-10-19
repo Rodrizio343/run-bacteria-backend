e11b3401e1be825fcfe21af4dab539deb25cf1c9
Rodrigo <rodrigo.martinez@webexport.com.ar>
2023-10-19 20:51:36 +0100

    fix - author name and avatar

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -1,7 +1,55 @@
+'use strict';
+
 /**
- * post controller
+ *  article controller
  */
 
-import { factories } from '@strapi/strapi'
+const { createCoreController } = require('@strapi/strapi').factories;
 
-export default factories.createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+
+    const query = strapi.db.query('api::post.post');
+
+    await Promise.all(
+      data.map(async (item, index) => {
+        const post = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy'],
+        });
+        data[index].createdBy = {
+          id: post.createdBy.id,
+          firstname: post.createdBy.firstname,
+          lastname: post.createdBy.lastname,
+          email: post.createdBy.email
+        };
+      })
+    );
+
+    return { data, meta };
+  },
+  async findOne(ctx) {
+    const { data } = await super.findOne(ctx);
+
+    const query = strapi.db.query('api::post.post');
+
+    const post =  await query.findOne({
+      where: {
+        id: data.id,
+      },
+      populate: ['createdBy'],
+    });
+    data.createdBy = {
+      id: post.createdBy.id,
+      firstname: post.createdBy.firstname,
+      lastname: post.createdBy.lastname,
+      email: post.createdBy.email
+    };
+    
+    return { data }   
+  } 
+}));
