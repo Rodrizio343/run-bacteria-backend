75712798575ee75cc364fc2c9b056a39f767936c
Rodrigo <rodrigo.martinez@webexport.com.ar>
2023-10-22 20:29:56 +0100

    fix post author data for category list

--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -1,7 +1,34 @@
+import { createCoreController } from '@strapi/strapi/lib/factories';
+
 /**
- * category controller
+ * item controller
  */
 
-import { factories } from '@strapi/strapi'
+module.exports = createCoreController('api::category.category', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { data } = await super.findOne(ctx);
 
-export default factories.createCoreController('api::category.category');
+    const query = strapi.db.query('api::category.category');
+    const queryPost = strapi.db.query('api::post.post');
+
+    const category =  await query.findOne({
+      where: {
+        id: data.id,
+      },
+      populate: ['posts'],
+    });
+    data['posts'] = category.posts
+    await Promise.all(
+      category.posts.map(async(item, index)=> {
+        const post = await queryPost.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['categories', 'image', 'createdBy'],
+        });
+        data.posts[index] = post
+      })
+    )
+    return { data }   
+  } 
+}));
