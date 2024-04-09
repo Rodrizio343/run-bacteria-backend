54df1d1d2114424d67ab796dd2bdf2a76abfaa79
Rodrigo <rodrigo.martinez@webexport.com.ar>
2024-04-09 16:27:04 +0100

    aws ses added

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,21 +1,35 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
         params: {
-          ACL: env('AWS_ACL', 'public-read'),
-          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-          Bucket: env('AWS_BUCKET'),
+          ACL: env("AWS_ACL", "public-read"),
+          signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+          Bucket: env("AWS_BUCKET"),
         },
       },
       actionOptions: {
         upload: {},
         uploadStream: {},
         delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: "amazon-ses",
+      providerOptions: {
+        key: env("AWS_ACCESS_KEY_ID"),
+        secret: env("AWS_ACCESS_SECRET"),
+        amazon: "https://email.eu-west-1.amazonaws.com",
+      },
+      settings: {
+        defaultFrom: env("EMAIL"),
+        defaultReplyTo: env("EMAIL"),
       },
     },
   },
